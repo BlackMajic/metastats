7462d510eb68ec483590e1480e39b2c33f9779e1
black_majic <black_majic@ddb75c36-e60d-0410-93a2-b60e19774e26>
2006-01-28 23:31:25 +0000

    Restructured execution, added command line params
    git-svn-id: http://metastats.svn.sourceforge.net/svnroot/metastats/trunk/metastats@7 ddb75c36-e60d-0410-93a2-b60e19774e26

--- daemon/metastats.pl
+++ daemon/metastats.pl
@@ -1,259 +1,437 @@
 #!/usr/bin/perl
-
+#
 # Metastats Listener Daemon
 # ------------------------------------------------
 # A statistical data logger for multiplayer games
 # Copyright (c) 2004-2006, Nick Thomson.
-
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-my $version = "00.00.57";
+#
+# $configfile - system path and name of metastats config file.
+#
+my $configfile = './metastats.conf';
 
-use Strict;
+#
+#
+################################################################################
+# No need to edit below this line
+#
+
+use strict;
+use POSIX;
 use Config;
-
-$Config{useithreads} or die "Metastats requires ithreads for its TCP support. Please recompile perl (5.8.1+) and then reload Metastats.";
-
+use Getopt::Long;
+use Time::Local;
+use Digest::MD5;
+use DBI;
 use IO::Socket;
 use threads;
-use DBI;
-use Digest::MD5;
-use Time::Local;
+Getopt::Long::Configure ("bundling");
 
+$|=1;
 $SIG{HUP} = \&sigHUP;
 $SIG{__DIE__} = \&sigDIE;
 
+##
+## Initialize variables & defaults
+##
+our $version = '00.00.59';
+our $db;
+my ($query, $result);
+my ($cl_help, $cl_version, $cl_stdin, $cl_server_ip, $cl_server_port);
 
-print <<EOI;
-
-Metastats Listener Daemon $version
+my $header = "\nMetastats Listener Daemon $version
 -----------------------------------------------
 A statistical data logger for multiplayer games
-Copyright (c) 2004-2006, Nick Thomson.
+http://metastats.sourceforge.net
+Copyright (c) 2004-2006 Nick Thomson.
+Copyright (c) 2006      Tim McLennan.
 
 Metastats comes with ABSOLUTELY NO WARRANTY. This
 is free software, you are welcome to redistribute
 it under certain conditions. For further details,
-please visit http://www.gnu.org/copyleft/gpl.html.
+please visit http://www.gnu.org/copyleft/gpl.html\n\n";
 
-EOI
+my $usage = "\nUsage: [cat PATH/*.log |] $0 [OPTIONS]...\n
+Recieve streamed logs from one or more gameservers
+to be parsed and inserted into a database of some sort.
 
-%cvars = &parseConfig('./metastats.conf');
+  -h, --help			Display this notice.
+  -V, --version			Display version information.
+  -d, --debug			Enable debug output (-dd = higher level)
+  -n, --nodebug			Disables debug output (-nn = lower level)
+  -m, --mode=MODE		Select tracking mode (Normal, NameTrack, Lan)
+  -v, --verbose			Print debug information to console.
+??-o, --output=FILE		Outputs debug data to FILE.
+  -s, --stdin			Read logs from <STDIN> instead of a stream.
+  -i, --server-ip		IP Address of the server from which the 
+				logs you are reading from <STDIN> came from.
+  -p, --server-port		Port of the server above.
+      --nostdin			Reads from stream.
+      --db-host=HOST		Database server ip address or hostname.
+      --db-port=PORT		Database server port.
+      --db-type=TYPE		Type of database to connect to.
+      --db-name=DATABASE	Database name to connect to.
+      --db-prefix=PREFIX	Table prefix in use on database.
+      --db-user=USERNAME	Database username.
+      --db-pass=PASSWORD	Database password.
+      --dns			Enable DNS hostname resolution.
+      --dns-timeout		Timout DNS queries after n seconds.
+  -l, --dns-locate		Enable GeoLocation DNS feature.
+      --nodns			Disables ALL DNS features.
+  -r, --rcon			Send rcon commands to logged servers.
+  -R, --norcon			Disables above.
+  -t, --timestamp		Use timestamp in logs.
+  -T, --notimestamp		Use timestamp on database server.
+  -u, --update-stable		Automatically update to a stable build.
+  -b, --update-beta		Automatically update to a beta build.
 
-require $cvars{CoreDir}.'/main.pm';
+Note: Options specified on the command line overwrite
+      options in $configfile.\n\n";
 
-print "   Checking for updates..\t\t";
-my (undef, $vmaj, $vmin, $vrev) = &updateGetVersion();
-my ($cmaj, $cmin, $crev) = split(/\./, $version);
-print "done.\n";
+our %conf;		# Setup default values
+$conf{CoreDir}		= './core';
+$conf{ModuleDir}	= './modules';
+$conf{UseTimestamp}	= 0;
+$conf{DBType}		= 'mysql';
+$conf{DBHost}		= 'localhost';
+$conf{DBPort}		= '3306';
+$conf{DBName}		= 'metastats';
+$conf{DBUser}		= 'metastats';
+$conf{DBPass}		= '';
+$conf{DBPrefix}		= 'ms';
+$conf{DBLowPriority}	= 0;
+$conf{UDPListenHost}	= '*';
+$conf{UDPListenPort}	= '27500';
+$conf{TCPListenHost}	= '*';
+$conf{TCPListenPort}	= '27500';
+$conf{LogLevel}		= 0;
+$conf{LogEcho}		= 1;
+$conf{LogFile}		= '';
+$conf{LogCloneDir}	= '';
+$conf{LogCloneOneFile}	= '';
+$conf{LogForward}	= '';
+$conf{CrashReport}	= './crash.txt';
+$conf{CrashNotify}	= '';
+$conf{UpdateStable}	= 0;
+$conf{UpdateBeta}	= 0;
 
-if ($cmaj < $vmaj)
-{
-	print "\n   ** A major update is available.\n\n";
-}
-elsif ($cmin < $vmin)
-{
-	print "\n   ** A minor update is available.\n\n";
-}
-elsif ($crev < $vrev)
-{
-	print "\n   ** An update is available.\n\n";
-}
-else
-{
-	print "\n   Metastats is up to date.\n\n";
-}
+# Read command line args
+GetOptions(
+	"help|h"	=> \$cl_help,
+	"version|V"	=> \$cl_version);
 
+die($usage)  if ($cl_help);
+die($header) if ($cl_version);
+
+#
+# Check Dependancies
+#
+$Config{useithreads} or die "Metastats $version requires ithreads for TCP support.Please recompile perl(5.8.1+) and then reload Metastats.\n\n";
+
+##
+## Begin Execution
+##
+print $header;				# Print Metastats information
+%conf = parseConfig($configfile);	# Parse our config
+require "$conf{CoreDir}/main.pm";	# Include required files
+
+#
+# Auto-update Metastats
+#
+updateMetastats();
+
+#
+# Connect to Database
+#
 print "   Connecting to database..\t\t";
-
-$db = DBI->connect(
-	"DBI:mysql:".$cvars{DatabaseName}.":".$cvars{DatabaseHost},
-	$cvars{DatabaseUser},
-	$cvars{DatabasePass}) or die ("fail.\n   Unable to connect to database:\n   -> $DBI::errstr\n");
-
-print "done.\n";
-
-# populate our cache from the database
-%servers = {};
-%server_ips = {};
-%games = {};
-%mods = {};
-%modifiers = {};
-%defs = {};
-my $res = '';
+$db = DBI->connect("DBI:$conf{DBType}:".$conf{DBName}.":".$conf{DBHost}.":".$conf{DBPort}, $conf{DBUser}, $conf{DBPass})
+		or die("[ fail ]\n   Unable to connect to database:\n->$DBI::errstr\n");
+print "[ done ]\n";
 
 #
-# Servers
+# Read DB Config
 #
-foreach $res (&dbQuery($db, "SELECT server_id, server_ip, server_port, server_name, game_id, mod_id, rcon FROM uastats_servers"))
-{
-	$servers{$res->{server_id}} = [ $res->{server_ip},
-						$res->{server_port},
-						$res->{server_name},
-						$res->{game_id},
-						$res->{mod_id},
-						$res->{rcon} ];
-
-	# Associate server ip:port with its ID for quicker checking
-	$server_ips{$res->{server_ip}.":".$res->{server_port}} = $res->{server_id};
+print "   Loading configuration data from db..\t";
+$query = "SELECT
+		keyname,
+		value
+	  FROM
+		$conf{DBPrefix}_core_config
+	  WHERE
+		ext='core'";
+foreach $result(dbQuery($db, $query)) {
+        $conf{$result->{key}} = $result->{value};
 }
-
-$res = '';
-foreach $res (&dbQuery($db, "SELECT game_id, game_plugin FROM uastats_games"))
-{
-	$games{$res->{game_id}} = $res->{game_plugin};
-}
-$res = '';
-foreach $res (&dbQuery($db, "SELECT mod_id, mod_plugin FROM uastats_mods"))
-{
-	$mods{$res->{mod_id}} = $res->{mod_plugin};
-}
+print "[ done ]\n";
 
 #
-# Definitions, modifiers follow :p
+# Read command line variables into %conf (overwrite)
+#
+GetOptions(
+	"help|h"		=> \$cl_help,
+	"version|V"		=> \$cl_version,
+	"debug|d+"		=> \$conf{LogLevel},
+	"nodebug|n+"		=> \$conf{LogLevel},
+	"mode|m=s"		=> \$conf{Mode},
+	"verbose|v"		=> \$conf{LogEcho},
+	"output|o=s"		=> \$conf{LogFile},
+	"stdin!"		=> \$cl_stdin,
+	"s"			=> \$cl_stdin,
+	"server-ip|i=s"		=> \$cl_server_ip,
+	"server-port|p=i"	=> \$cl_server_port,
+	"db-host=s"		=> \$conf{DBHost},
+	"db-port=s"		=> \$conf{DBPort},
+	"db-type=s"		=> \$conf{DBType},
+	"db-name=s"		=> \$conf{DBName},
+	"db-prefix=s"		=> \$conf{DBPrefix},
+	"db-username=s"		=> \$conf{DBUser},
+	"db-password=s"		=> \$conf{DBPass},
+	"dns!"			=> \$conf{DNSResolveIP},
+	"dns-timeout=i"		=> \$conf{DNSTimeout},
+	"dns-locate|l"		=> \$conf{DNSGeoLocate},
+	"rcon!"			=> \$conf{RconEnable},
+	"r"			=> \$conf{RconEnable},
+	"timestamp!"		=> \$conf{UseTimestamp},
+	"t"			=> \$conf{UseTimestamp}
+) or die($usage);
+
+# Setup listen ips/ports if no/bad value exists
+$conf{UDPListenHost} = "0.0.0.0" if ($conf{UDPListenHost} == "*" || !$conf{UDPListenHost});
+$conf{UDPListenPort} = 27500 if (!($conf{UDPListenPort}));
+$conf{TCPListenHost} = "0.0.0.0" if ($conf{TCPListenHost} == "*" || !$conf{TCPListenHost});
+$conf{TCPListenPort} = 27500 if (!($conf{TCPListenPort}));
+
+#
+# Include Modules
 #
 my @mods = ();
-%handlers = {};
-%players = {};
+my %handlers = {};
+my %players = {};
 
-print "\n";
+loadModules();
 
-# Search for Game Support Modules and load them
-opendir (MDIR, $cvars{ModuleDir}) or die ("Can't open game modules directory.");
-while ($file = readdir(MDIR))
-{
-	if ($file =~ /\.gsm$/)
-	{
-		# Require it
-		require $cvars{ModuleDir}.'/'.$file;
+#
+# Get a list of our tracked servers
+#
+my %servers = {};
+my %server_ips = {};
+my %games = {};
+my %mods = {};
+my %modifiers = {};
+my %defs = {};
 
-		# Initialise it
-		$file =~ s/\.gsm$//;
-		$file->init();
-		$handlers{$file} = {};
-		$players{$file} = {};
-	}
-	# We don't want to load MOD support until all the
-	# game support files have been initialised.
-	push @mods, $file if ($file =~ /\.msm$/);
-}
+getTrackedServers();
 
-closedir DIR;
+#
+# Create UDP Socket
+#
+print "   Creating UDP listen socket..\t\t";
+my $lsocket = IO::Socket::INET->new(Proto=>"udp",
+		LocalHost=>$conf{UDPListenHost},
+		LocalPort=>$conf{UDPListenPort})
+		or die("[ fail ]\n\nCould not create UDP listening socket:\n->$@\n");
+print "[ done ]\n";
+print "\n   Listening on port $conf{UDPListenPort}(UDP).\n\n";
 
-foreach $modsup (@mods)
-{
-	require $cvars{ModuleDir}.'/'.$modsup;
-	$modsup =~ s/\.msm$//;
-	$modsup->init();
-	$handlers{$modsup} = {};
-}
-
-print "\n";
-
-# Defaults if no value exists
-$cvars{UDPListenHost} = "*" if (!($cvars{UDPListenHost}));
-$cvars{UDPListenPort} = 27500 if (!($cvars{UDPListenPort}));
-
-print "   Creating UDP listening socket..\t";
-
-
-## Any spare cheese?
-
-my $lsocket = IO::Socket::INET->new(	Proto=>"udp",
-							LocalHost=>"192.168.17.10",
-							LocalPort=>$cvars{UDPListenPort})
-				or die ("fail.\n\nCould not create UDP listening socket:\n->$@\n");
-
-print "done.\n";
-print "\n   Listening on port $cvars{UDPListenPort} (UDP).\n\n";
-
+#
+# Create TCP Socket
+#
 print "   Creating TCP listener thread..\t";
-my $tcpThread = threads->new(\&tcpListener);
+my $tcpThread = threads->new(\&tcpListener());
 
-for (;;)
-{
+##
+## Main Loop
+##
+for (;;) {
 	my $incdata = "";
 	$lsocket->recv($incdata, 1024);
-	$l_remote_host = $lsocket->peerhost;
-	$l_remote_port = $lsocket->peerport;
+	my $l_remote_host = $lsocket->peerhost;
+	my $l_remote_port = $lsocket->peerport;
 	my $serverid = $server_ips{$l_remote_host.":".$l_remote_port};
-	if ($serverid)
-	{
+	if ($serverid) {
 		print "Recognised server\n";
-	}
-	else
-	{
+	} else {
 		print "Unrecognised server\n";
 	}
 	$games{$servers{$serverid}[3]}->parse($serverid, $mods{$servers{$serverid}[4]}, $incdata);
-
 }
 
+##
+## Functions
+##
 
 # void tcpListener ()
+#
 # Thread to handle the TCP listening socket
+#
 sub tcpListener
 {
-	print "done.\n   Creating TCP listening socket..\t";
-	my $tlsocket = IO::Socket::INET->new(	Proto=>"tcp",
-								Listen=>1,
-								Reuse=>1,
-								LocalHost=>"192.168.17.10",
-								LocalPort=>$cvars{TCPListenPort})
-				or die("fail.\n\nCould not create TCP listening socket:\n->$@\n");
-
-	print "done.\n";
-	print "\n   Listening on port $cvars{TCPListenPort} (TCP).\n\n";
-
-	for (;;)
-	{
+	print "[ done ]\n   Creating TCP listen socket..\t\t";
+	my $tlsocket = IO::Socket::INET->new(Proto=>"tcp",
+						Listen=>1,
+						Reuse=>1,
+						LocalHost=>$conf{TCPListenHost},
+						LocalPort=>$conf{TCPListenPort})
+				or die("[ fail ]\n\nCould not create TCP listening socket:\n->$@\n");
+	print "[ done ]\n";
+	print "\n   Listening on port $conf{TCPListenPort}(TCP).\n\n";
+	for (;;) {
 		my $tlsock_new = $tlsocket->accept();
 		print "   ** Incoming TCP connection from ".$tlsock_new->peerhost.":".$tlsock_new->peerport."..\n";
 		print "   Launching new worker thread..\n";
 		my $tlnID = threads->new(\&tcpWorker($tlsock_new));
 	}
-	
-	
 }
 
 # void tcpWorker (object socket)
+#
 # Takes accepted connections from the listener thread and actually
 # does something with them
+#
 sub tcpWorker
 {
-
 	my ($sockid) = @_;
 	my $incdata = " ";
-
 	while ($sockid->recv($incdata, 1024))
 	{
 		print "-> $incdata\n";
 	}
 	print "   Worker thread exiting\n";
-
 }
 
-## Will code for food.
-
-sub sigHUP
+# hash parseConfig (string configFile)
+#
+# Open the config file, and parse it's contents
+# into a hash
+#
+sub parseConfig
 {
-	$SIG{HUP} = \&sigHUP;
-	# Reload config, update cache et al
+	my ($configFile) = @_;
+	my %cv = undef;
+	
+	print "   Loading configuration file..\t\t";
+	if (-e $configFile) {
+		open (cfg, $configFile) || dieNicely("Cannot open $configFile");
+		while (<cfg>) {
+			chomp;
+			my ($ln) = $_;
+			if (!(/^(\040|\t)*#/)) {
+				# remove double whitespace characters
+				$ln =~ s/(\s{2,})/\040/g;
+				
+				# remove comments at the end of lines
+				$ln =~ s/(#.*)$//;
+				
+				# Match any number of letters followed by a space
+				# then any number of characters inside quotes
+				# OR any number of digits
+				$ln =~ /^([a-zA-Z]+)\040((\"(.+)\")|((\d)+))$/;
+				# $1 and $2 now contain variable and it's contents
+				my ($var, $val) = ($1, $2);
+				$var =~ s/\"//; $var =~ s/\"$//;
+				$val =~ s/\"//; $val =~ s/\"$//;
+				
+				$cv{$var} = $val if (($var) && ($val));
+			}
+		}
+		print "[ done ]\n";
+	} else {
+		print "[ fail ]\n\n";
+		die "The configuration file was not found.\nPlease check that $configFile exists.";
+	}
+	return %cv;
 }
 
-sub sigDIE
+# void loadModules()
+#
+# Open the modules directory and require *.msm; set up handlers
+# then require *.mem and setup their handlers
+#
+sub loadModules
 {
-	# Tidy up!
+	# Include Support Modules
+	print "\n   Loading Support Modules..\n";
+	opendir (MDIR, $conf{ModuleDir}) or die ("[ fail ]\nCan't open support modules directory.\n\n");
+	while (my $file = readdir(MDIR)) {
+		if ($file =~ /\.msm$/) {
+			# Require it
+			require $conf{ModuleDir}.'/'.$file;
+			
+			# Initialise it
+			$file =~ s/\.msm$//;
+			$file->init();
+			$handlers{$file} = {};
+			$players{$file} = {};
+		}
+		# We don't want to load MOD support until all the
+		# game support files have been initialised.
+		push @mods, $file if ($file =~ /\.mem$/);
+	}
+	closedir DIR;
+	print "   Completed Loading Modules.\n\n";
+	
+	# Include Extension Modules
+	print "   Loading Extension Modules..\n";
+	my $mod;
+	foreach $mod (@mods) {
+		require $conf{ModuleDir}.'/'.$mod;
+		$mod =~ s/\.mem$//;
+		$mod->init();
+		$handlers{$mod} = {};
+	}
+	print "   Completed Loading Extensions\n\n";
+}
+
+# void getTrackedServers()
+#
+# SELECT the servers stored in the database, save them into a hash
+# then load them into their game/mod hash respectively.
+#
+sub getTrackedServers
+{
+	print "   Identifying Tracked Servers..\t";
+	$query = "SELECT
+			server_id,
+			server_ip,
+			server_port,
+			server_name,
+			server_mod,
+			server_rcon
+		  FROM
+			$conf{DBPrefix}_core_servers";
+	
+	foreach $result (&dbQuery($db, $query)) {
+		$servers{$result->{server_id}} = [ $result->{server_ip},
+						$result->{server_port},
+						$result->{server_name},
+						$result->{server_mod},
+						$result->{server_rcon} ];
+		# Associate server ip:port with its ID for quicker checking
+		$server_ips{$result->{server_ip}.":".$result->{server_port}} = $result->{server_id};
+	}
+	$result = '';
+	
+	foreach $result (&dbQuery($db, "SELECT ext, ext_name FROM $conf{DBPrefix}_core_extensions")) {
+		$games{$result->{ext}} = $result->{ext_name};
+	}
+	$result = '';
+	
+	foreach $result (&dbQuery($db, "SELECT id, ext FROM $conf{DBPrefix}_core_mods")) {
+		$mods{$result->{id}} = $result->{ext};
+	}
+	print "[ done ]\n";
 }
