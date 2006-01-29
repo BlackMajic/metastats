63c7ee1ebe861e9b509189ca81d82988db52bf56
black_majic <black_majic@ddb75c36-e60d-0410-93a2-b60e19774e26>
2006-01-29 09:48:39 +0000

    moved parseConfig to metastats.pl, created updateMetastats.
    git-svn-id: http://metastats.svn.sourceforge.net/svnroot/metastats/trunk/metastats@8 ddb75c36-e60d-0410-93a2-b60e19774e26

--- daemon/core/main.pm
+++ daemon/core/main.pm
@@ -2,21 +2,21 @@
 # ---------------------------------------------------------
 # A statistical data logger for multiplayer games
 # Copyright (c) 2004-2006, Nick Thomson.
-
+# 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# 
 # ################################################
 # No  modifications are  nessecary  to this  file;
 # please  see   the  documentation  and  edit  the
@@ -26,45 +26,56 @@
 
 
 # array dbQuery (object dbObject; string dbQuery)
-#
+# 
 # Queries the database specified in dbObject
 # recieves results, and places them in an array of hashref's
 # returned to the caller.
 #
 sub dbQuery
 {
-	my $dbObject = @_[0];
-	my $dbQuery = @_[1];
+	my ($dbObject, @queries) = @_;
 	my @results = ();
 	my $row = '';
-
-	my $dbStatement = $dbObject->prepare($dbQuery) or die("Unable to prepare query");
-	$dbStatement->execute() or die ("Unable to execute statement");
-	while ($row = $dbStatement->fetchrow_hashref())
-	{
-		push @results, $row;
-	}	
-
+	my $i;
+	
+	foreach $i ($queries) {
+		$dbQuery = (split /[^\\];/, $queries[$i])[0]; # Stop multipul queries from happening in one string
+		
+		# Escape SQL statement
+		#$dbQuery =~ s/\\/\\\\/g;	# replace \ with \\
+		#$dbQuery =~ s/'/\\'/g;		# replace ' with \'
+		#$dbQuery =~ s/\;/\\\;/g;	# replace ; with \;
+		
+		my $dbStatement = $dbObject->prepare($dbQuery)
+					or die("Unable to prepare query");
+		$dbStatement->execute() or die ("Unable to execute statement");
+		while ($row = $dbStatement->fetchrow_hashref()) {
+			push @results, $row;
+		}
+	}
+	@queries = ();
 	return @results;
-
 }
 
+# updateCache ()
+#
 sub updateCache
 {
 
 
 }
 
+# array updateGetVersion ()
+# Queries the update server, and returns a 3 value array containing the 
+# major, minor, and revision versions respectively.
 #
 sub updateGetVersion
 {
-
-	$updateserver = "jumpingmad.com";
+	$updateserver = "metastats.sourceforge.net";
 	$updateport = "80";
-	$updateurl = "/autoupdate/index.php";
-
-
-	$http = IO::Socket::INET->new(Proto=>"tcp",PeerAddr=>$updateserver,PeerPort=>$updateport,Reuse=>1) or die "[FAIL]\n";
+	$updateurl = "/update.php";
+	
+	$http = IO::Socket::INET->new(Proto=>"tcp",PeerAddr=>$updateserver,PeerPort=>$updateport,Reuse=>1) or die "[ fail ]\n";
 	$http->autoflush(1);
 	send ($http,"GET $updateurl HTTP/1.0\nHost: $updateserver\nConnection: keep-alive\n\n",0);
 	while (<$http>)
@@ -72,75 +83,40 @@ sub updateGetVersion
 		$ln = $_;
 		chop $ln;
 		$ln =~ s/\s//g;
-		if ($ln =~ /^UAS\|(\d\d)\.(\d\d)\.(\d\d)/)
+		if ($ln =~ /^MSV\|(\d\d)\.(\d\d)\.(\d\d)/)
 		{
 			($maj, $min, $rev) = ($1, $2, $3);
 		}
 	}
 	close ($http);
-
+	
 	my @res_ver = ();
-	@res_ver[1] = $maj;
-	@res_ver[2] = $min;
-	@res_ver[3] = $rev;
-
+	@res_ver[0] = $maj;
+	@res_ver[1] = $min;
+	@res_ver[2] = $rev;
+	
 	return @res_ver;
-
 }
 
-# void parseConfig (string configFile)
-#
-# Open the config file, and parse it's contents
-# into a hash
-#
-sub parseConfig
+sub updateMetastats
 {
-
-	my ($configFile) = @_;
-
-	my %cv = undef;
-
-	print "   Loading configuration file..\t\t";
-	if (-e $configFile)
-	{
-		open (cfg, $configFile) || &dieNicely("test");
-
-		while (<cfg>)
-		{
-			chomp;
-			my ($ln) = $_;
-			if (!(/^(\040|\t)*#/))
-			{		
-				# remove double whitespace characters
-				$ln =~ s/(\s{2,})/\040/g;
-
-				# remove comments at the end of lines
-				$ln =~ s/(#.*)$//;
-
-				# Match any number of letters followed by a space
-				# then any number of characters inside quotes
-				# OR any number of digits
-				$ln =~ /^([a-zA-Z]+)\040((\"(.+)\")|((\d)+))$/;
-
-				# $1 and $2 now contain variable and it's contents
-				($var, $val) = ($1, $2);
-
-				$var =~ s/\"//; $var =~ s/\"$//;
-				$val =~ s/\"//; $val =~ s/\"$//; 
-
-				$cv{$var} = $val if (($var) && ($val));
-			}
+	print "   Checking for updates..\t\t";
+	if ($main::conf{UpdateStable} or $main::conf{UpdateBeta}) {
+		my ($vmaj, $vmin, $vrev) = &updateGetVersion();
+		my ($cmaj, $cmin, $crev) = split(/\./, $main::version);
+		print "[ done ]\n";
+		if ($cmaj < $vmaj) {
+			print "     ** A major update is available.\n";
+		} elsif ($cmin < $vmin) {
+			print "     ** A minor update is available.\n";
+		} elsif ($crev < $vrev) {
+			print "     ** An update is available.\n";
+		} else {
+			print "     Metastats is up to date.\n";
 		}
-		print "done.\n";
+	} else {
+		print "[ skip ]\n"
 	}
-	else
-	{
-		print "failed.\n\n";
-		die "The configuration file was not found.\nPlease check that $configFile exists.";
-	}
-
-	return %cv;
-
 }
 
 1;
